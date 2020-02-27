ff16f8b7c12369de1553aaa56bb1ab3c4feb542b
jungraejang <jungraejang@pursuit.org>
2020-02-27 00:45:41 -0500

    heroku deployment 15

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const transactionsRouter = require("./routes/transactions");
 
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -23,10 +23,10 @@ app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "/../frontend/build")));
 
 app.use(
   session({
@@ -45,7 +45,7 @@ app.use("/api/market", marketRouter);
 app.use("/api/portfolio", portfolioRouter);
 app.use("/api/transactions", transactionsRouter);
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
 });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
